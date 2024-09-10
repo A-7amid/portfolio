e6ecfc042770d31d6f5ad7a964b08350ce3441f5
Hi <hello@ww.com>
2024-09-10 22:52:08 +0300

    Inetial commit

---
+++ src/onclick.js
@@ -0,0 +1,14 @@
+const github = () => {
+    window.open('https://github.com/Abdelhameed710', '_blank')
+}
+
+const linkedin = () => {
+    window.open('https://www.linkedin.com/in/abdelhamed-ahmed-9a9715302/s', '_blank')
+}
+
+const twitter = () => {
+    window.open('https://x.com/abdelhamed29258', '_blank')
+}
+
+// ========
+
---
+++ src/write.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#auto-type", {
+    strings: ["HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED", "HI THERE!", "I'M ABDUL HAMEED",],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    cursorChar: "_",
+})
+
+
+
